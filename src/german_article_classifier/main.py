fd33c8e15f7be0782efcd43f0006dc7ff363ab0d
volkovwashere <volkovkrisztian@gmail.com>
2021-11-14 22:56:15 +0100

    asd

--- src/german_article_classifier/main.py
+++ src/german_article_classifier/main.py
@@ -1,4 +1,4 @@
 from german_article_classifier.datapipeline.pre_processing_pipeline import run_pre_processing_pipeline
 
 df_test = run_pre_processing_pipeline(dataset_level=2)
-print(df_test.head(5))
+print(df_test.head(4))
